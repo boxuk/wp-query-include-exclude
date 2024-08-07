7f53763e5a9d21fff347ab856a6f79c838b01035
boxuk-wp-robot <developers@boxuk.com>
2024-08-07 10:47:48 +0000

    Merge pull request #80 from boxuk/dependabot/npm_and_yarn/wordpress/icons-10.4.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '49a7e68f155d0a2848c5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '0c0c96927e6959eb773d');
